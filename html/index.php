df2edf991d0eb6a04e377fec77442f5604e359a7
Justin McCombs <jtmccombs@gmail.com>
2015-09-02 11:29:47 -0400

    move buttons

--- html/index.php
+++ html/index.php
@@ -45,66 +45,63 @@
 <body>
 
 <div class="container contentCell">
-	<div class="col-xs-7">
-		<div class="gamePanel">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="gamePanel">
 
-			<div class="gameElements">
-				<div class="ttyOutput scoreOutput" id="scoreDiv"></div>
-				<div class="ttyOutput linesOutput" id="linesDiv"></div>
-				<div class="ttyOutput levelOutput" id="levelDiv"></div>
-				<div class="ttyOutput tickerOutput" id="tickerDiv"></div>
+				<div class="gameElements">
+					<div class="ttyOutput scoreOutput" id="scoreDiv"></div>
+					<div class="ttyOutput linesOutput" id="linesDiv"></div>
+					<div class="ttyOutput levelOutput" id="levelDiv"></div>
+					<div class="ttyOutput tickerOutput" id="tickerDiv"></div>
+					<div class="buttonControls" style="position: relative; top: 10px; left: 22px; z-index: 99999;">
+						<button class="btn btn-xs btn-primary" id="upLevelButton">Level Up</button>
+						<button class="btn btn-xs btn-primary" id="downLevelButton">Level Down</button>
+					</div>
+					<div class="gameEndOutputHidden" id="gameEndContainer">
+						<div style="padding:5px;" id="gameEndDiv"></div>
+					</div>
 
-				<div class="gameEndOutputHidden" id="gameEndContainer">
-					<div style="padding:5px;" id="gameEndDiv"></div>
+					<canvas class="gameCanvas" id="gameCanvas" width="600" height="500" onclick="onClickEvent(event)" class="gameCanvas">Your browser does not natively support Html5, or the Canvas Tag. Using this browser is slowing the progress of the web. Please get a modern browser, such as Google Chrome or Mozzila FireFox</canvas>
 				</div>
+			</div>
+		</div>
+		<div class="col-xs-10 col-xs-offset-1">
+			<div class="controlsTitle">
+				<br/>
+				<b>Controls:</b><br/>
+				<table border="1" cellpadding="3" class="controlsTable">
 
-				<canvas class="gameCanvas" id="gameCanvas" width="600" height="500" onclick="onClickEvent(event)" class="gameCanvas">Your browser does not natively support Html5, or the Canvas Tag. Using this browser is slowing the progress of the web. Please get a modern browser, such as Google Chrome or Mozzila FireFox</canvas>
+					<tr class="controlsTableHeader">
+						<td>Move Block</td>
+						<td><span id="shiftLeft">Left</span>, <span id="shiftRight">Right</span></td>
+					</tr>
+					<tr class="controlsTableHeader">
+						<td>Soft Drop</td>
+						<td><span id="softDrop">Down</span></td>
+					</tr>
+					<tr class="controlsTableHeader">
+						<td>Rotate</td>
+						<td><span id="rotateLeft">Z</span>, <span id="rotateRight">X, Up</span></td>
+					</tr>
+					<tr class="controlsTableHeader">
+						<td>Save Piece</td>
+						<td><span id="swap">C, Shift</span></td>
+					</tr>
+					<tr class="controlsTableHeader">
+						<td>Hard Drop</td>
+						<td><span id="hardDrop">Space</span></td>
+					</tr>
+					<tr class="controlsTableHeader">
+						<td>Pause</td>
+						<td>Esc</td>
+					</tr>
+
+				</table>
 			</div>
 		</div>
 	</div>
-	<div class="col-xs-4">
-		<div class="controlsTitle">
-			<br/>
-			<b>Controls:</b><br/>
-			<table border="1" cellpadding="3" class="controlsTable">
-				<tr>
-					<td colspan="2">
-						<button class="btn" id="upLevelButton">Level Up</button>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<button class="btn" id="downLevelButton">Level Down</button>
-					</td>
-				</tr>
-				<tr class="controlsTableHeader">
-					<td>Move Block</td>
-					<td><span id="shiftLeft">Left</span>, <span id="shiftRight">Right</span></td>
-				</tr>
-				<tr class="controlsTableHeader">
-					<td>Soft Drop</td>
-					<td><span id="softDrop">Down</span></td>
-				</tr>
-				<tr class="controlsTableHeader">
-					<td>Rotate</td>
-					<td><span id="rotateLeft">Z</span>, <span id="rotateRight">X, Up</span></td>
-				</tr>
-				<tr class="controlsTableHeader">
-					<td>Save Piece</td>
-					<td><span id="swap">C, Shift</span></td>
-				</tr>
-				<tr class="controlsTableHeader">
-					<td>Hard Drop</td>
-					<td><span id="hardDrop">Space</span></td>
-				</tr>
-				<tr class="controlsTableHeader">
-					<td>Pause</td>
-					<td>Esc</td>
-				</tr>
 
-			</table>
-		</div>
-	</div>
 </div>
 
 </body>
